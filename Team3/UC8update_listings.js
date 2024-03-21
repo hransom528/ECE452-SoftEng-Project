dae947827b2f47d45cec898154075e9d75af71d2
shrey dobariya <dobariyashrey@gmail.com>
2024-03-20 23:13:05 -0400

    UC8

--- Team3/UC8update_listings.js
+++ Team3/UC8update_listings.js
@@ -1,11 +1,17 @@
-// UC8update_listings.js
 const { ObjectId } = require('mongodb');
-const { connectDB } = require('../dbConfig.js'); // Update the path as necessary
+const { connectDB } = require('../dbConfig'); // Update the path as necessary
+const { v4: uuidv4 } = require('uuid'); // Import the UUID package
 
 async function updateListings(productIds, updateFields) {
     if (!productIds || !updateFields) {
         throw new Error('\'productIds\' and \'updateFields\' are required');
     }
+
+    // Generate a UUID for this update operation
+    const updateId = uuidv4();
+    
+    // Include the UUID in the update fields
+    updateFields.updateId = updateId;
 
     const objectIdProductIds = productIds.map(id => new ObjectId(id));
     const db = await connectDB();
@@ -17,8 +23,9 @@ async function updateListings(productIds, updateFields) {
     const result = await collection.updateMany(filter, update);
     console.log(`${result.matchedCount} listings matched the filter criteria`);
     console.log(`${result.modifiedCount} listings were updated`);
-
-    return result;
+    
+    // Return the result along with the UUID for this operation
+    return { result, updateId };
 }
 
 module.exports = {
