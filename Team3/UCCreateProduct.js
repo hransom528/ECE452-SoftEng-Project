602dcb399bd54b051e1fdfecc45574f19dc24936
tripatdaksh <tripatdakshkhetarpaul@gmail.com>
2024-03-23 11:22:47 -0400

    changes to add product

--- Team3/UCCreateProduct.js
+++ Team3/UCCreateProduct.js
@@ -1,14 +1,13 @@
 const { ObjectId } = require('mongodb');
 const { connectDB } = require('../dbConfig'); // Update the path as necessary
-const { v4: uuidv4 } = require('uuid'); // Import the UUID package
+
 
 async function addProduct(productDetails) {
     if (!productDetails) {
         throw new Error('\'productDetails\' are required');
     }
 
-    // Optionally add a UUID to each product for unique identification
-    productDetails.productId = uuidv4();
+    
 
     const db = await connectDB();
     const collection = db.collection('products');
