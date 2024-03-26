dd7a56a8385cff33cd29fd8fff57ccdcd13a2017
Aseef Durrani <aseef.durrani11@gmail.com>
2024-03-25 22:08:36 -0400

    message

--- Team1/ChatBot/fetchProducts.js
+++ Team1/ChatBot/fetchProducts.js
@@ -6,20 +6,27 @@ async function fetchAllProducts() {
   const collection = db.collection("products");
 
   try {
+    // console.log("yea ok buddy");
     // Fetch all products but exclude the _id and images fields
-    const products = await collection.find({}, { projection: { _id: 0, images: 0 } }).toArray();
+    const products = await collection
+      .find({}, { projection: { _id: 0, images: 0 } })
+      .toArray();
 
     // Save to a JSON file
     await new Promise((resolve, reject) => {
-      fs.writeFile("products.json", JSON.stringify(products, null, 2), (err) => {
-        if (err) {
-          console.error("Error writing to file:", err);
-          reject(err);
-        } else {
-          console.log("Products saved to products.json successfully!");
-          resolve();
+      fs.writeFile(
+        "Team1/ChatBot/products.json",
+        JSON.stringify(products, null, 2),
+        (err) => {
+          if (err) {
+            console.error("Error writing to file:", err);
+            reject(err);
+          } else {
+            console.log("Products saved to products.json successfully!");
+            resolve();
+          }
         }
-      });
+      );
     });
   } catch (error) {
     console.error("An error occurred while fetching products:", error);
@@ -29,4 +36,4 @@ async function fetchAllProducts() {
   }
 }
 
-fetchAllProducts();
+module.exports = { fetchAllProducts };
